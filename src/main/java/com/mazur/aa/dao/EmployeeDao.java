1bf66d835da19751dba94ba65dc888ee684dd587
Khan <fuck@mail>
2015-10-13 11:05:36 +0300

    first working release

---
+++ src/main/java/com/mazur/aa/dao/EmployeeDao.java
@@ -0,0 +1,18 @@
+package com.mazur.aa.dao;
+
+import java.util.List;
+import com.mazur.aa.model.Employee;
+ 
+public interface EmployeeDao {
+ 
+    Employee findById(int id);
+ 
+    void saveEmployee(Employee employee);
+     
+    void deleteEmployeeBySsn(String ssn);
+     
+    List<Employee> findAllEmployees();
+ 
+    Employee findEmployeeBySsn(String ssn);
+ 
+}
--- src/main/webapp/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h2>Hello, everybody!</h2>
-</body>
-</html>
